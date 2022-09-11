a5685ae0b13315f641351ad61a5daef66e64145e
David Hoepelman <992153+dhoepelman@users.noreply.github.com>
2022-09-11 20:42:02 +0200

    Figuring out LEDs and uasyncio
    Signed-off-by: David Hoepelman <992153+dhoepelman@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,19 @@
+# Rasberry pico digital clock & thermometer
+
+I recently bought a cheap clock + thermometer from a store.
+It looks quite nice, but only works ~12h on 3xAA battery and the thermometer is wildly innacurate.
+Good excuse to replace the internals with something made myself!
+
+Hardware:
+
+- Rasberry Pico W
+- DS3231 RTC (clock)
+- AM2320 Temperature & Humidity sensor
+- CN3065 BNC + Li-on 18650 battery
+- Shell + LED matrix from ["Blokker Stella"](https://www.blokker.nl/blokker-wekker-stella---15x8x9-cm/2385214.html)
+  clock
+
+Software:
+
+- Microptyon
+- NTP for time
--- main.py
+++ main.py
@@ -1,11 +1,108 @@
 from machine import Pin
+from uasyncio import Task, create_task, run, sleep_ms
 
-import time
+#########
+# Config
+#########
 
-led = Pin(25, Pin.OUT)
+NTP_HOST = "pool.ntp.org"
+WIFI_SSID = 'mw32-guest'
+WIFI_PW = 'mw32-guest'
 
-while True:
-    led(1)
-    time.sleep(1)
-    led(0)
-    time.sleep(1)
+#########
+# Constants
+#########
+
+WLAN_STAT_GOT_IP = 3
+
+#########
+# State & initialization
+#########
+
+
+leds = {
+    'onboard': Pin('LED', Pin.OUT),
+    'red': Pin(18, Pin.OUT),
+    'green': Pin(17, Pin.OUT),
+    'blue': Pin(16, Pin.OUT),
+}
+timer: dict[str, Task] = {
+    'onboard': None,
+    'red': None,
+    'green': None,
+    'blue': None,
+}
+# wdt = WDT(timeout=8388)  # Watch dog against crash
+
+#########
+# LED
+#########
+
+led_periods = {
+    '0_boot': 50,
+    '1_wifi': 250,
+    '2_ntp': 500,
+}
+
+
+def led_blink(period: int, which: str = 'onboard') -> None:
+    led: Pin = leds[which]
+    print(f"LED\t- {which}\t- {period}ms")
+
+    if timer[which] is not None:
+        timer[which].cancel()
+
+    async def blink():
+        led.off()
+        while True:
+            await sleep_ms(period)
+            led.toggle()
+
+    timer[which] = create_task(blink())
+
+
+#########
+# WLAN
+#########
+
+# wlan = network.WLAN(network.STA_IF)
+# wlan.active(True)
+# led_blink('1_wifi')
+# wlan.connect(WIFI_SSID, WIFI_PW)
+
+
+# def await_wifi() -> bool:
+#     while not wlan.isconnected():
+#         print(f"Connecting wifi... {wlan.status()}")
+#         sleep_ms(1000)
+#     if wlan.status() == 3:
+#         print(f"Connected wifi: {wlan.ifconfig()}")
+#         return True
+#     else:
+#         print(f"WLAN collection failed with status {wlan.status()}")
+#         return False
+
+
+#########
+# NTP
+#########
+
+# run(await_wifi())
+# await_wifi()
+
+# TODO: disable WLAN
+
+
+#########
+# Application
+#########
+
+async def main():
+    led_blink(led_periods['0_boot'], 'onboard')
+    led_blink(led_periods['1_wifi'], 'red')
+    while True:
+        print("main")
+        await sleep_ms(500)
+
+
+run(main())
